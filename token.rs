a965f5b210ec863ed0a7f81d04bd87e5257f2fa7
Akshay R <akshayrajan345@gmail.com>
2024-08-07 22:39:40 +0530

    Add token program

---
+++ token.rs
@@ -0,0 +1,152 @@
+// Import dependencies
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+    metadata::{
+        create_metadata_accounts_v3,
+        mpl_token_metadata::types::DataV2,
+        CreateMetadataAccountsV3, 
+        Metadata as Metaplex,
+    },
+};
+
+// Declare Program ID (SolPG will automatically update this when you deploy)
+declare_id!("BKmAqLyTu37SEpN1fy7kHSmNNPX9giYk6DP3NSWfwKPF");
+
+// Define the program and instructions
+#[program]
+mod token_minter {
+    use super::*;
+    pub fn init_token(ctx: Context<InitToken>, metadata: InitTokenParams) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+        let token_data: DataV2 = DataV2 {
+            name: metadata.name,
+            symbol: metadata.symbol,
+            uri: metadata.uri,
+            seller_fee_basis_points: 0,
+            creators: None,
+            collection: None,
+            uses: None,
+        };
+
+        let metadata_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                payer: ctx.accounts.payer.to_account_info(),
+                update_authority: ctx.accounts.mint.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint_authority: ctx.accounts.mint.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+            &signer
+        );
+
+        create_metadata_accounts_v3(
+            metadata_ctx,
+            token_data,
+            false,
+            true,
+            None,
+        )?;
+
+        msg!("Token mint created successfully.");
+
+        Ok(())
+    }
+
+    pub fn mint_tokens(ctx: Context<MintTokens>, quantity: u64) -> Result<()> {
+        let seeds = &["mint".as_bytes(), &[ctx.bumps.mint]];
+        let signer = [&seeds[..]];
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &signer,
+            ),
+            quantity,
+        )?;
+
+        Ok(())
+    }
+}
+
+// Define the context for each instruction
+#[derive(Accounts)]
+#[instruction(
+    params: InitTokenParams
+)]
+pub struct InitToken<'info> {
+    /// CHECK: New Metaplex Account being created
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    #[account(
+        init,
+        seeds = [b"mint"],
+        bump,
+        payer = payer,
+        mint::decimals = params.decimals,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+}
+
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    #[account(
+        mut,
+        seeds = [b"mint"],
+        bump,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = payer,
+        associated_token::mint = mint,
+        associated_token::authority = payer,
+    )]
+    pub destination: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
+
+// Define the init token params
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct InitTokenParams {
+    pub name: String,
+    pub symbol: String,
+    pub uri: String,
+    pub decimals: u8,
+}
+
+// Running tests...
+//   anchor.test.ts:
+//   Test Minter
+//     Already initiated! Skipping this test.
+//     ✔ initialize (243ms)
+//       https://explorer.solana.com/tx/4LhZ3sSoJaozEViczMhPAaLztYNqvAnnsQAAa5fD7E5Pgnr3pvsYenSJr2zzzWxmAvxJxEXaxByjpqWJwqzQXboF?cluster=devnet
+//     Token ID (Mint Address): 45VJCPVMwEW5jPcQH2w6YBoBP4hj4jb65UZFLvqsp1Qt
+//     Metadata Account: ERitp2KBvrmZW77L6zpZLdPX2NX2ThG5xketcyBJojFe
+//     Token Balance: 70
+//     ✔ mint tokens (7676ms)
+//   2 passing (8s)
